6bb7d4e8755df0efc5beb69f944fbd81ddf61616
github username <github email address>
2017-06-30 00:40:56 -0400

    win seasons

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import SearchedShowReducer from './reducer_searched_show'
 import ShowsReducer from './reducer_shows'
+import SeasonsReducer from './reducer_seasons'
 
 const rootReducer = combineReducers({
   searchedShow: SearchedShowReducer,
   shows: ShowsReducer,
+  seasons: SeasonsReducer
 });
 
 export default rootReducer;
